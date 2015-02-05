d4b6950b06696a87ef7d3b71039ff0fa0aa696d0
philicious <the.cypher@gmail.com>
2015-02-05 13:33:19 +0100

    version bump due to forgotten file

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'the.cypher@gmail.com'
 license          'GNU GPL v2'
 description      'Installs/Configures a Spacewalk client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
