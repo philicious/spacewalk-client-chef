f0dcc987df34c2fcd6619822a1ef302b1388348e
philicious <the.cypher@gmail.com>
2017-03-22 20:23:41 +0100

    bump version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,10 @@
 name             'spacewalk-client'
 maintainer       'Phil Schuler'
-maintainer_email 'the.cypher@gmail.com'
+maintainer_email 'schuler.philipp@gmail.com'
 license          'GNU GPL v2'
 description      'Installs/Configures a Spacewalk client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.12'
+version          '0.2.0'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
