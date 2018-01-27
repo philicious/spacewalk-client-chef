31ab585d0dfa9581fe310c844e3050d5c0bbd0c3
Martin Olsen <molsen@kyriba.com>
2018-01-26 16:42:27 -0800

    Adding more robust checks for registration. Removing added repo files.

--- recipes/rhel.rb
+++ recipes/rhel.rb
@@ -1,6 +1,65 @@
 arch = node['kernel']['machine'] == 'x86_64' ? 'x86_64' : 'i386'
 platform_major = node['platform_version'][0]
 
+do_register = false
+
+# check if up2date file has correct serverURL value
+up2date = '/etc/sysconfig/rhn/up2date'
+serverurl_value = "serverURL=#{node['spacewalk']['reg']['server']}/XMLRPC"
+if File.exist?(up2date)
+  unless IO.read(up2date).include?(serverurl_value)
+    Chef::Log.info('Wrong serverURL in up2date file, registering.')
+    do_register = true
+  end
+end
+
+# check if systemid file exists
+systemid = '/etc/sysconfig/rhn/systemid'
+unless File.exist?(systemid)
+  Chef::Log.info('Missing systemid file, registering.')
+  do_register = true
+end
+
+# if saving key
+key_dir = node['spacewalk']['reg']['key_dir']
+key_file = "#{key_dir}/key"
+if node['spacewalk']['reg']['save']
+  directory key_dir do
+    recursive true
+    owner 'root'
+    group 'root'
+    mode '600'
+    not_if { File.directory? key_dir }
+  end
+  if !do_register && File.exist?(key_file) && node['spacewalk']['reg']['key'] == IO.read(key_file)
+    Chef::Log.info("System has already been registered.")
+    return
+  end
+  file key_file do
+    content node['spacewalk']['reg']['key']
+    owner 'root'
+    group 'root'
+    mode '600'
+    action :create
+  end
+  unless do_register
+    Chef::Log.info("Missing reg_file or incorrect key_value in reg_file, registering.")
+    do_register = true
+  end
+end
+
+# Keep inventory of existing repos
+etc_yum_repos_d = '/etc/yum.repos.d'
+original_repos = []
+ruby_block 'remove_repo_files' do
+  block do
+    Dir.foreach(etc_yum_repos_d) do |repo|
+      next if repo == '.' or repo == '..'
+      original_repos << repo
+    end
+  end
+end
+
 remote_file "#{Chef::Config[:file_cache_path]}/spacewalk-client-repo.rpm" do
   source "#{node['spacewalk']['rhel']['base_url']}/#{platform_major}/#{arch}/spacewalk-client-repo-#{node['spacewalk']['rhel']['repo_version']}.el#{platform_major}.noarch.rpm"
   action :create
@@ -55,9 +114,9 @@ if node['spacewalk']['enable_osad']
   end
 
   execute 'register-with-spacewalk-server' do
-    command "rhnreg_ks --activationkey=#{node['spacewalk']['reg']['key']} --serverUrl=#{node['spacewalk']['reg']['server']}/XMLRPC"
-    not_if { (File.exist?('/etc/sysconfig/rhn/systemid')) }
+    command "rhnreg_ks --activationkey=#{node['spacewalk']['reg']['key']} --serverUrl=#{node['spacewalk']['reg']['server']}/XMLRPC --force"
     notifies :restart, 'service[osad]'
+    only_if { do_register }
   end
 
   service 'osad' do
@@ -66,7 +125,22 @@ if node['spacewalk']['enable_osad']
   end
 else
   execute 'register-with-spacewalk-server' do
-    command "rhnreg_ks --activationkey=#{node['spacewalk']['reg']['key']} --serverUrl=#{node['spacewalk']['reg']['server']}/XMLRPC"
-    not_if { (File.exist?('/etc/sysconfig/rhn/systemid')) }
+    command "rhnreg_ks --activationkey=#{node['spacewalk']['reg']['key']} --serverUrl=#{node['spacewalk']['reg']['server']}/XMLRPC --force"
+    only_if { do_register }
+  end
+end
+
+# remove repos that were added by this recipe
+if node['spacewalk']['dont_add_repo_files']
+  ruby_block 'remove_repo_files' do
+    block do
+      Dir.foreach(etc_yum_repos_d) do |repo|
+        next if repo == '.' or repo == '..'
+        if !original_repos.include? repo
+          Chef::Log.info("Removing repo file: #{repo}")
+          File.delete("#{etc_yum_repos_d}/#{repo}")
+        end
+      end
+    end
   end
 end
